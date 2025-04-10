55c6d95ad2022a9be8a478ef954eedb89a160cfe
Emir Pesereklü <emir.peserekli@gmail.com>
2025-04-10 23:42:13 +0330

    Updated some methods in "Database" class

--- src/db/Database.java
+++ src/db/Database.java
@@ -11,7 +11,7 @@ public class Database {
 
     public static void add(Entity e) {
         e.id = entities.size() + 1 ;
-        entities.add(e);
+        entities.add(e.copy());
     }
     public static Entity get(int id) {
         for (int e : deletedIds) {
@@ -21,7 +21,7 @@ public class Database {
         }
         for (Entity e : entities) {
             if (e.id == id) {
-                return e;
+                return e.copy();
             }
         }
         throw new EntityNotFoundException(id);
@@ -39,7 +39,7 @@ public class Database {
     public static void update(Entity e) {
         for (int i = 0; i < entities.size(); i++) {
             if (entities.get(i).id == e.id) {
-                entities.set(i,e);
+                entities.set(i,e.copy());
                 return;
             }
             throw new EntityNotFoundException(e.id);
