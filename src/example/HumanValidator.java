c5cd8a835acbec167fae21ea1ab14b70a7eeb715
Emir Pesereklü <emir.peserekli@gmail.com>
2025-04-11 16:11:30 +0330

    Created HumanValidator class

---
+++ src/example/HumanValidator.java
@@ -0,0 +1,10 @@
+package example;
+import db.*;
+import db.exception.InvalidEntityException;
+
+public class HumanValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        // Code for human validation
+    }
+}
