cf7cc40ea18495c4254ca8f34c1e9921962b63ae
Emir Pesereklü <emir.peserekli@gmail.com>
2025-04-11 15:59:34 +0330

    Added age field to Human class

--- src/example/Human.java
+++ src/example/Human.java
@@ -4,15 +4,18 @@ import db.Entity;
 
 public class Human extends Entity {
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Override
     public Human copy() {
-        Human copyHuman = new Human(name);
+        Human copyHuman = new Human(name, age);
         copyHuman.id = id;
+        copyHuman.age = age;
 
         return copyHuman;
     }
